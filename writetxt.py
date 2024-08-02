8c18ce4f6afd8ccce68ededcf2b00f47047afd86
SeungPyo Jeon <54004940+SeungPyo-Jeon@users.noreply.github.com>
2024-08-02 11:08:46 +0900

    updata file

--- writetxt.py
+++ writetxt.py
@@ -1,2 +1,9 @@
+print('original file')
 with open('a.txt','r') as f:
     print(f.read())
+
+with open('a.txt','w') as f:
+    f.write("nice to me you to")
+print('written file')
+with open('a.txt','r') as f:
+    print(f.read())    
