504028f2dd654afb314060626eb233d03c76d412
SeungPyo Jeon <54004940+SeungPyo-Jeon@users.noreply.github.com>
2025-04-25 23:00:27 +0900

    Delete writetxt.py

--- writetxt.py
+++
@@ -1,9 +0,0 @@
-print('original file')
-with open('a.txt','r') as f:
-    print(f.read())
-
-with open('a.txt','w') as f:
-    f.write("nice to me you to")
-print('written file')
-with open('a.txt','r') as f:
-    print(f.read())    
